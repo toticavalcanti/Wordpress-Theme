e81c916e934274abc8ffe379a1c27e61925130d5
toticavalcanti <toticavalcanti@hotmail.com>
2015-11-03 18:31:39 -0200

    Atualizacoes index.php e style.css

--- quero_ir_para_noruega/index.php
+++ quero_ir_para_noruega/index.php
@@ -8,8 +8,53 @@
 	
 	<?php wp_head(); ?>
 	</head>
+
 	<body>
-		
+		<div id="header">
+
+			<div id="top-header">
+
+				<div id="top-header-content">
+
+					<div id="header-pages">
+						 p&aacute;ginas do cabeçalho
+					</div><!--/ endheader-pages -->
+
+					<div id="header-social">
+						 Parte de redes sociais no cabe&ccedil;alho
+					</div><!--/ end header-social -->
+
+				</div><!--/ end top-header-content -->
+				
+			</div><!--/ end top-header -->
+
+			<div id="header-content">
+				
+				<div id="logo">
+					Logo
+				</div><!--/ end logo -->
+
+				<div id="login">
+					Login
+				</div><!--/ end login -->
+
+				<div id="search">
+					Barra de busca
+				</div><!--/ end search -->
+
+			</div><!--/ end header-content -->
+
+			<div id="nav">
+
+				<div id="nav-content">
+					Barra de navega&ccedil;&atilde;o
+				</div><!--/ end nav-content -->
+
+			</div><!--/ end nav -->
+
+		</div><!--/ end header -->
+
 	<?php wp_footer(); ?>
 	</body>
+
 </html>
