f2584756078fc9e79c694c309c577bd7d53eff8a
toticavalcanti <toticavalcanti@hotmail.com>
2015-11-07 17:48:40 -0200

    update wordpress_themes

--- quero_ir_para_noruega/README.txt
+++ quero_ir_para_noruega/README.txt
@@ -1,7 +1,7 @@
 Construction of wordpress theme for the Quero Ir Para a Noruega blog.
 
 Obs. 
-The screenshot.png file must has 600 px width and 460 px height
+The screenshot.png file must has 600 px width and 460 px height.
 
 ---------------------------------------------------------------------------------------
 Construção de um tema wordpress para o blog Quero Ir Para a Noruega
--- quero_ir_para_noruega/index.php
+++ quero_ir_para_noruega/index.php
@@ -16,9 +16,9 @@
 
 				<div id="top-header-content">
 
-					<div id="header-pages">
+					<div id="header-paginas">
 						 p&aacute;ginas do cabeçalho
-					</div><!--/ endheader-pages -->
+					</div><!--/ endheader-paginas -->
 
 					<div id="header-social">
 						 Parte de redes sociais no cabe&ccedil;alho
@@ -53,6 +53,69 @@
 			</div><!--/ end nav -->
 
 		</div><!--/ end header -->
+
+		<div id="container">
+			
+			<div id="content">
+				<div id="destaque">Destaque</div>
+				<div id="viagens">Viagens</div><!--/ end viagens-->
+				<div id="idioma">Idioma</div><!--/ end idioma-->
+				<div id="cultura">Cultura</div><!--/ end cultura-->
+				<div id="educacao">Educa&ccedil;&atilde;o</div><!--/ end educacao-->
+				<div id="videos">V&iacute;deos</div><!--/ end videos-->
+				<div id="anuncie">Anuncie</div><!--/ end anuncie-->
+
+			</div><!--/ end content -->
+
+		<div id="sidebar">
+			
+			<div id="sidebar-vistos">
+				mais sistos
+			</div><!--fim sidebar-vistos -->
+
+			<div id="sidebar-colunistas">
+				sidebar colunistas
+			</div><!--fim sidebar-vistos -->
+
+			<div id="sidebar-publi">
+				publicidades
+			</div><!--fim sidebar-publi -->
+
+			<div id="sidebar-coment">
+				mais comentados
+			</div><!--fim sidebar-coment -->
+
+			<div id="sidebar-facebook">
+				facebook
+			</div><!--fim sidebar-facebook -->
+
+		</div><!--fim side-bar-->
+
+		</div><!--/ end container -->
+
+		<div="footer">
+			
+			<div id="footer-content">
+				
+				<div id="footer-sobre">
+					sobre n&oacute;s
+				</div><!--fim footer-sobre-->
+
+				<div id="footer-paginas">
+					p&aacute;ginas
+				</div><!--fim footer-paginas-->
+
+				<div id="footer-recentes">
+					posts recentes
+				</div><!--fim footer-recentes-->
+
+			</div><!--fim footer-content-->
+
+		<div id="footer-info">
+			footer info
+		</div><!--fim footer-info-->
+
+		</div><!--/ end footer -->
 
 	<?php wp_footer(); ?>
 	</body>
